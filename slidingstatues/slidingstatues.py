f1401982fd6d575b91b0abf3d28dc8e4cf2ba18b
Sean <sean@prestigecode.com>
2017-11-29 01:33:43 -0500

    Revised

--- slidingstatues/slidingstatues.py
+++ slidingstatues/slidingstatues.py
@@ -3,7 +3,6 @@
 # In the actual dungeoneering room, there are 4 boards, in this case, we will create a class
 # that we will create multiple instances of, and then compare the two correlating boards to
 # confirm a matching statue positions
-
 class Board:
     #Our board can be any size, the actual size is 5x5
 
@@ -11,6 +10,26 @@ class Board:
     def __init__(self, size):
         #set up board size
         self.size = size
-        #assign coordinates for each statue, we cannot have a statue on the same spot
-        self.statue1 = [0,0]
-        self.statue2 = [0,1]
+
+
+class Statue:
+
+    #initial, if the x and y are defined
+    #we can also define the type as a number, 0 for white statue, 1 for black, by default None until assigned
+    def __init__(self, x, y):
+        self.type = None
+        self.x = x
+        self.y = y
+
+    #if they are not defined, this should assign to the default position
+    def Statue(self):
+        self.x = 0
+        self.y = 0
+
+    def moveStatue(self, x, y):
+        self.x = x
+        self.y = y
+        return (x, y)
+
+    def setType(self, input):
+        self.type = input
